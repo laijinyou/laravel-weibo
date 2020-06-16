9fb1e29b540a2bd600bd5ec0727226c057c1f291
Sloth <1183820@qq.com>
2020-06-16 09:45:12 +0900

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
@@ -9,5 +10,16 @@ class Status extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
     }
 }
